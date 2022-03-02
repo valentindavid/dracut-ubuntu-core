d18c1cb717cd320e1bb9626b8eed8859ba886158
Valentin David <valentin.david@canonical.com>
2022-03-02 16:40:01 +0100

    Extract "server" feature into a module
    This will be enabled by default the same way as in core-initrd

--- modules/core-modules/module-setup.sh
+++ modules/core-modules/module-setup.sh
@@ -7,11 +7,7 @@ depends() {
 }
 
 install() {
-    for conf in                                 \
-        ubuntu-core-initramfs.conf              \
-        ubuntu-core-server.conf
-    do
-        instmods $(cat "${moddir}/${conf}" | grep -v "^#")
-        inst_simple "${moddir}/${conf}" "usr/lib/modules-load.d/${conf}"
-    done
+    conf=ubuntu-core-initramfs.conf
+    instmods $(cat "${moddir}/${conf}" | grep -v "^#")
+    inst_simple "${moddir}/${conf}" "usr/lib/modules-load.d/${conf}"
 }
--- modules/core/module-setup.sh
+++ modules/core/module-setup.sh
@@ -4,4 +4,9 @@ check() {
 
 depends() {
     echo base dm crypt systemd systemd-initrd udev-rules busybox core-modules core-mounts core-bootchart core-journald-console core-recovery-chooser-trigger core-shell core-default-hostname
+    case "${kernel}" in
+	*-generic)
+	    echo core-feature-server
+	    ;;
+    esac
 }
