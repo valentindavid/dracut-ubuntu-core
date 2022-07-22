1c1c38fbd6c6f4098d23e6c43029f0163c8ef050
Valentin David <valentin.david@canonical.com>
2022-07-22 16:51:44 +0200

    Sync with core-initrd

--- modules/core-modules/module-setup.sh
+++ modules/core-modules/module-setup.sh
@@ -10,4 +10,11 @@ install() {
     conf=ubuntu-core-initramfs.conf
     instmods $(cat "${moddir}/${conf}" | grep -v "^#")
     inst_simple "${moddir}/${conf}" "usr/lib/modules-load.d/${conf}"
+
+    # raspi USB OTG driver, needed for jammy
+    instmods dwc2
+
+    # All HID drivers should be available. This is for
+    # recovery-chooser-trigger to be able to read keyboards.
+    instmods =drivers/hid
 }
--- modules/core-recovery-chooser-trigger/module-setup.sh
+++ modules/core-recovery-chooser-trigger/module-setup.sh
@@ -9,6 +9,9 @@ depends() {
 install() {
     inst_simple "${systemdsystemunitdir}"/snapd.recovery-chooser-trigger.service
     inst_simple "${moddir}/survive-switch-root.conf" "${systemdsystemunitdir}"/snapd.recovery-chooser-trigger.service.d/survive-switch-root.conf
+    inst_simple "${moddir}/recovery-chooser-trigger-switch-root" /usr/lib/recovery-chooser-trigger-switch-root
+    inst_simple "${moddir}/recovery-chooser-trigger-switchroot.service" "${systemdsystemunitdir}"/recovery-chooser-trigger-switchroot.service
 
     systemctl -q --root "$initdir" add-wants basic.target snapd.recovery-chooser-trigger.service
+    systemctl -q --root "$initdir" add-wants initrd-switch-root.target recovery-chooser-trigger-switchroot.service
 }
